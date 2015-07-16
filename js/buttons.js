373a275dbd899ba5a4a7b777fbba2fd8e6c5cc68
Keith <Keith@KB>
2015-07-16 16:52:19 -0600

    Changed Yelp response formating code to independent function.

--- js/buttons.js
+++ js/buttons.js
@@ -34,7 +34,33 @@ function userQuery() {
         //document.getElementById("json").innerHTML = response;
         saveToLocal(response);
         var yelpObj = JSON.parse(response);
-        formatYelpResponse(yelpObj);
+        if (yelpObj.length === 0) {
+            document.getElementById("output").innerHTML = "Your search returned no results. Please try again.";
+        }
+        else {
+            var output = "";
+            for (var i = 0; i < yelpObj.businesses.length; i++) {
+                output += ""
+                        + "<img src=\"" + yelpObj.businesses[i].image_url + "\">"
+                        + "</br>"
+                        + yelpObj.businesses[i].name
+                        + "</br>"
+                        + "<img src=\"" + yelpObj.businesses[i].rating_img_url_large + "\">"
+                        + "</br>"
+                        + yelpObj.businesses[i].review_count + " reviews"
+                        + "</br>"
+                        + "<a href=\"" + yelpObj.businesses[i].url + "\">Read reviews on Yelp</a>"
+                        + "</br>"
+                        + yelpObj.businesses[i].display_phone
+                        + "</br>"
+                        + yelpObj.businesses[i].location.display_address[0]
+                        + "</br>"
+                        + yelpObj.businesses[i].location.display_address[1]
+                        + "</br>"
+                        + "</br>";
+            }
+            document.getElementById("output").innerHTML = output;
+        }
     });
 
 
